a989b915612363f6792a4e644ea52ff6632f4812
JRTell <john-robin@jrtell.se>
2018-07-01 15:00:18 +0200

    Removed chart.js and moment import

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-import moment from 'moment'
-import Chart from 'chart.js'
 
 Vue.config.productionTip = false;
 
